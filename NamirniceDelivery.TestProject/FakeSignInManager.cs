4876340fc08de0095c8aabdcf0a20654a6aaf7cf
adleto <tarik.halilovic@hotmail.com>
2019-12-23 20:29:40 +0100

    Dodan test za kategorija pregled.

---
+++ NamirniceDelivery.TestProject/FakeSignInManager.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using NamirniceDelivery.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamirniceDelivery.TestProject
+{
+    public class FakeSignInManager : SignInManager<ApplicationUser>
+    {
+        public FakeSignInManager()
+                : base(new FakeUserManager(),
+                     new Mock<IHttpContextAccessor>().Object,
+                     new Mock<IUserClaimsPrincipalFactory<ApplicationUser>>().Object,
+                     new Mock<IOptions<IdentityOptions>>().Object,
+                     new Mock<ILogger<SignInManager<ApplicationUser>>>().Object,
+                     new Mock<IAuthenticationSchemeProvider>().Object,
+                     new Mock<IUserConfirmation<ApplicationUser>>().Object
+                     )
+        { }
+    }
+
+
+
+    public class FakeUserManager : UserManager<ApplicationUser>
+    {
+        public FakeUserManager()
+            : base(new Mock<IUserStore<ApplicationUser>>().Object,
+              new Mock<IOptions<IdentityOptions>>().Object,
+              new Mock<IPasswordHasher<ApplicationUser>>().Object,
+              new IUserValidator<ApplicationUser>[0],
+              new IPasswordValidator<ApplicationUser>[0],
+              new Mock<ILookupNormalizer>().Object,
+              new Mock<IdentityErrorDescriber>().Object,
+              new Mock<IServiceProvider>().Object,
+              new Mock<ILogger<UserManager<ApplicationUser>>>().Object)
+        { }
+
+        public override Task<IdentityResult> CreateAsync(ApplicationUser user, string password)
+        {
+            return Task.FromResult(IdentityResult.Success);
+        }
+
+        public override Task<IdentityResult> AddToRoleAsync(ApplicationUser user, string role)
+        {
+            return Task.FromResult(IdentityResult.Success);
+        }
+
+        public override Task<string> GenerateEmailConfirmationTokenAsync(ApplicationUser user)
+        {
+            return Task.FromResult(Guid.NewGuid().ToString());
+        }
+
+    }
+}
---
+++ NamirniceDelivery.TestProject/Sprint_1_Test.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NamirniceDelivery.Data.Entities;
+using NamirniceDelivery.Services.Interfaces;
+using NamirniceDelivery.Web.Controllers;
+using NamirniceDelivery.Web.ViewModels.AdministrativniRadnik;
+using System.Collections.Generic;
+
+namespace NamirniceDelivery.TestProject
+{
+    [TestClass]
+    public class Sprint_1_Test
+    {
+        Mock<IPopust> popustService;
+        FakeSignInManager signInManager;
+        Mock<IKategorija> kategorijaService;
+        Mock<INamirnica> namirnicaService;
+
+        public Sprint_1_Test()
+        {
+            popustService = new Mock<IPopust>();
+            signInManager = new FakeSignInManager();
+            kategorijaService = new Mock<IKategorija>();
+            namirnicaService = new Mock<INamirnica>();
+        }
+
+        [TestMethod]
+        public void Pregled_Kategorija_Test()
+        {
+            kategorijaService.Setup(k => k.GetKategorije()).Returns(new List<Kategorija> {
+                new Kategorija()
+                {
+                    Id = 1,
+                    Naziv = "Voće"
+                },
+                new Kategorija()
+                {
+                    Id = 2,
+                    Naziv = "Povrće"
+                }
+            });
+            AdministrativniRadnikController c = new AdministrativniRadnikController(signInManager,kategorijaService.Object, namirnicaService.Object, popustService.Object);
+            ViewResult v = c.PregledKategorija() as ViewResult;
+            PregledKategorijaViewModel p = v.Model as PregledKategorijaViewModel;
+            Assert.AreEqual(p.KategorijaList.Count, 2);
+        }
+    }
+}
