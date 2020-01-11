a1c6efc93cbff0a92f066e1522312036b42f7442
Arnela Manov <arnela.manov@edu.fit.ba>
2020-01-11 15:53:50 +0100

    Dodaj get i post funkciju za prikaz login viewa #100 i #101

--- NamirniceDelivery.Web/Controllers/AccountController.cs
+++ NamirniceDelivery.Web/Controllers/AccountController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Identity.UI.V3.Pages.Account.Internal;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using NamirniceDelivery.Data.Entities;
-//using NamirniceDelivery.Web.ViewModels.Account;
+using NamirniceDelivery.Web.ViewModels.Account;
 
 namespace NamirniceDelivery.Web.Controllers
 {
@@ -27,6 +27,63 @@ namespace NamirniceDelivery.Web.Controllers
             _logger = logger;
             _emailSender = emailSender;
         }
+        public async Task<IActionResult> Login(string returnUrl = null)
+        {
+            var model = new LoginVM
+            {
+                ErrorMessage = ""
+            };
+            if (!string.IsNullOrEmpty(model.ErrorMessage))
+            {
+                ModelState.AddModelError(string.Empty, model.ErrorMessage);
+            }
 
+            returnUrl = returnUrl ?? Url.Content("~/");
+
+            // Clear the existing external cookie to ensure a clean login process
+            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+
+            //ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            model.ReturnUrl = returnUrl;
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM model)
+        {
+            model.ReturnUrl = model.ReturnUrl ?? Url.Content("~/");
+
+            if (ModelState.IsValid)
+            {
+                // This doesn't count login failures towards account lockout
+                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+                var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberMe, lockoutOnFailure: true);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User logged in.");
+                    return LocalRedirect("/Home/Index");
+                }
+                //if (result.RequiresTwoFactor)
+                //{
+                //    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
+                //}
+                if (result.IsLockedOut)
+                {
+                    _logger.LogWarning("Nalog zaključan.");
+                    return RedirectToPage("./Lockout"); //dodaj lockout
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Pogrešan username ili lozinka.");
+                    return View(model);
+                }
+            }
+            // If we got this far, something failed, redisplay form
+            return RedirectToAction(nameof(Login));
+        }
+        public IActionResult Register()
+        {
+            return Ok();
+        }
     }
 }
