516904b6de93cb2e49daa9b43718312bd56c9250
Arnela Manov <arnela.manov@edu.fit.ba>
2020-01-11 15:51:44 +0100

    Napravi ViewModel za Login View #103

--- NamirniceDelivery.Web/ViewModels/Account/LoginVM.cs
+++ NamirniceDelivery.Web/ViewModels/Account/LoginVM.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NamirniceDelivery.Web.ViewModels.Account
+{
+    public class LoginVM
+    {
+        public string ReturnUrl { get; set; }
+        public string ErrorMessage { get; set; }
+        [Required(ErrorMessage = "Korisničko ime mora biti uneseno.")]
+        [Display(Name = "Korisničko ime")]
+        public string Username { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Lozinka mora biti unesena.")]
+        [Display(Name = "Lozinka")]
+        public string Password { get; set; }
+
+        [Display(Name = "Zapamti me?")]
+        public bool RememberMe { get; set; }
+    }
+}
